5667f602f3b87c8cca1c1261e41a17cddeb34370
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-06 22:53:40 +0300

    chore: CRUD create remote codex shit.

--- src/.helpers/flattenAndLinkValues/index.ts
+++ src/.helpers/flattenAndLinkValues/index.ts
@@ -6,11 +6,10 @@ import type {
 export function flattenAndLinkValues<T>(crListReplica: CRListReplica<T>): void {
   crListReplica.size = 0
   const resolvedSiblingPredecessors = new Set<string>()
-  const tombstonedEntries = new Set<NonNullable<DoublyLinkedListEntry<T>>>()
   for (const entry of Object.values(crListReplica.parentMap)) {
     if (!entry) continue
-    if (crListReplica.tombstones.has(entry.uuidv7)) tombstonedEntries.add(entry)
     if (
+      crListReplica.tombstones.has(entry.uuidv7) ||
       !isUuidV7(entry.uuidv7) ||
       (entry.predecessor !== '\0' && !isUuidV7(entry.predecessor))
     ) {
@@ -99,40 +98,6 @@ export function flattenAndLinkValues<T>(crListReplica: CRListReplica<T>): void {
     }
     resolvedSiblingPredecessors.add(predecessorIdentifier)
     crListReplica.cursor = entry
-  }
-  for (const entry of tombstonedEntries) {
-    const entrySiblings = crListReplica.childrenMap[entry.predecessor]
-    if (entry.prev) entry.prev.next = entry.next
-    if (entry.next) {
-      const next = entry.next
-      const nextPredecessor = entry.prev?.uuidv7 ?? '\0'
-      const oldNextPredecessor = next.predecessor
-      next.prev = entry.prev
-      next.predecessor = nextPredecessor
-      if (oldNextPredecessor !== nextPredecessor) {
-        const oldNextSiblings = crListReplica.childrenMap[oldNextPredecessor]
-        const nextSiblings = crListReplica.childrenMap[nextPredecessor]
-        if (Array.isArray(oldNextSiblings))
-          crListReplica.childrenMap[oldNextPredecessor] =
-            oldNextSiblings.filter((sibling) => sibling?.uuidv7 !== next.uuidv7)
-        if (Array.isArray(nextSiblings)) {
-          if (!nextSiblings.some((sibling) => sibling?.uuidv7 === next.uuidv7))
-            nextSiblings.push(next)
-        } else {
-          crListReplica.childrenMap[nextPredecessor] = [next]
-        }
-      }
-    }
-    if (Array.isArray(entrySiblings))
-      crListReplica.childrenMap[entry.predecessor] = entrySiblings.filter(
-        (sibling) => sibling?.uuidv7 !== entry.uuidv7
-      )
-    if (crListReplica.cursor === entry) {
-      crListReplica.cursor = entry.next ?? entry.prev
-    }
-    entry.prev = undefined
-    entry.next = undefined
-    delete crListReplica.parentMap[entry.uuidv7]
   }
   crListReplica.size = Object.keys(crListReplica.parentMap).length
 }

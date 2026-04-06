37433e9d51243cc10d728b61de17c2d16d7026b1
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-06 20:51:41 +0300

    chore: CRUD create handwritten beauty

--- src/.helpers/flattenAndLinkValues/index.ts
+++ src/.helpers/flattenAndLinkValues/index.ts
@@ -3,6 +3,10 @@ export function flattenAndLinkValues<T>(crListReplica: CRListReplica<T>): void {
   crListReplica.size = 0
   for (const entry of Object.values(crListReplica.parentMap)) {
     if (!entry) continue
+    if (crListReplica.tombstones.has(entry.uuidv7)) {
+      delete crListReplica.parentMap[entry.uuidv7]
+      continue
+    }
     crListReplica.cursor = entry
     const predecessorIdentifier = entry.predecessor
     const predecessor = crListReplica.parentMap[predecessorIdentifier]
@@ -25,11 +29,13 @@ export function flattenAndLinkValues<T>(crListReplica: CRListReplica<T>): void {
     let prev = predecessor
     let nextIndex = 0
     for (const sibling of siblings) {
+      const safeSibling = crListReplica.parentMap[sibling.uuidv7]
+      if (!safeSibling) continue
       nextIndex++
-      sibling.next = siblings[nextIndex]
-      sibling.prev = prev
-      prev.next = sibling
-      prev = sibling
+      safeSibling.next = crListReplica.parentMap[siblings[nextIndex].uuidv7]
+      safeSibling.prev = prev
+      prev.next = safeSibling
+      prev = safeSibling
     }
   }
   if (crListReplica.cursor) crListReplica.size++

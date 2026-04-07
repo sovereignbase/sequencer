f69c95a8cd01b996cc11ab9d0ec37d325030edfa
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-07 19:27:35 +0300

    chore: testing

--- src/.helpers/walkToIndex/index.ts
+++ src/.helpers/walkToIndex/index.ts
@@ -4,14 +4,6 @@ import type {
 } from '../../.types/index.js'
 import { CRListError } from '../../.errors/class.js'
 
-const walker = {
-  forward<T>(cursor: DoublyLinkedListEntry<T>) {
-    return cursor?.next
-  },
-  backward<T>(cursor: DoublyLinkedListEntry<T>) {
-    return cursor?.prev
-  },
-}
 export function walkToIndex<T>(
   targetIndex: number,
   crListReplica: CRListReplica<T>
@@ -20,10 +12,8 @@ export function walkToIndex<T>(
     throw new CRListError('INDEX_OUT_OF_BOUNDS', 'Index out of bounds')
   if (!crListReplica.cursor)
     throw new CRListError('LIST_EMPTY', 'List is empty')
-  const direction =
-    crListReplica.cursor.index > targetIndex ? 'backward' : 'forward'
-  const walk = walker[direction]
+  const direction = crListReplica.cursor.index > targetIndex ? 'prev' : 'next'
   while (crListReplica.cursor && crListReplica.cursor.index !== targetIndex) {
-    crListReplica.cursor = walk<T>(crListReplica.cursor)
+    crListReplica.cursor = crListReplica.cursor[direction]
   }
 }

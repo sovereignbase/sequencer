589658bb38efdd32f837d21bafbce3adb205f6a9
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-09 20:18:15 +0300

    chore: debugging

--- src/.helpers/assertListIndices/index.ts
+++ src/.helpers/assertListIndices/index.ts
@@ -2,11 +2,12 @@ import type { CRListReplica } from '../../.types/index.js'
 
 export function assertListIndices<T>(crListReplica: CRListReplica<T>): void {
   if (!crListReplica.cursor) return
-  let index = crListReplica.size - 1
+  let index = crListReplica.size
   while (crListReplica.cursor.next)
     crListReplica.cursor = crListReplica.cursor.next
 
-  while (index >= 0) {
+  while (index >= 1) {
+    index--
     crListReplica.cursor.index = index
     if (crListReplica.cursor.prev === undefined) break
     crListReplica.cursor = crListReplica.cursor.prev

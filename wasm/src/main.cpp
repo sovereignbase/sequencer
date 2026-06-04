fc4fc41658aced0f12134093920d1de7b547db98
Jori Lethinen <lehtinenjori03@gmail.com>
2026-06-04 17:34:00 +0300

    chore: plan cpp impl

--- wasm/src/main.cpp
+++ wasm/src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
+#include "unordered_map"
+
 extern "C" {
 
 int crlist_version_major() { return crlist_wasm::version_major; }
@@ -14,5 +16,14 @@ std::int32_t crlist_add(std::int32_t left, std::int32_t right) {
   return left + right;
 }
 
+void overwrite(){
+  return;
 }
+
+void merge(std::int32_t a,std::int32_t b,std::int32_t c,std::int32_t d,std::int32_t length) {
+
+return;
+}
+}
+
 

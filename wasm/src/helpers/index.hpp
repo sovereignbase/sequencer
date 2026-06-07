1ea8a82b42ebe1b366201a1ca08f6e9ed11c1c79
Jori Lethinen <lehtinenjori03@gmail.com>
2026-06-07 22:17:43 +0300

    chore: start wasm snapshot helper development

--- wasm/src/helpers/index.hpp
+++ wasm/src/helpers/index.hpp
@@ -78,7 +78,7 @@ std::uint32_t absolute_distance(std::uint32_t left, std::uint32_t right) {
 
 Key key_offset(Key key, std::uint32_t offset);
 
-Range *snapshot_range_at(State *state, std::uint32_t range_index) {
+Range *range_at(State *state, std::uint32_t range_index) {
   Range *range = state ? state->first : nullptr;
   for (std::uint32_t index = 0; range && index < range_index; index++)
     range = range->next_range;

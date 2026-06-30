e0ad9c586755f849271d7915f2842460ddfcffdb
Jori Lethinen <lehtinenjori03@gmail.com>
2026-06-30 16:57:01 +0300

    chore: fix timecode comparison

--- wasm/src/helpers/compare_timecode/index.hpp
+++ wasm/src/helpers/compare_timecode/index.hpp
@@ -6,9 +6,9 @@ std::int8_t compare_timecode(const Timecode *left, const Timecode *right) {
   std::uint8_t lane;
 
   for (lane = 0; lane < 4; lane++) {
-    if (left[lane] < right[lane])
+    if ((*left)[lane] < (*right)[lane])
       return -1;
-    if (left[lane] > right[lane])
+    if ((*left)[lane] > (*right)[lane])
       return 1;
   };
 

f47928bf251d3746caef56ca68ab635c9f5f4e1d
Jori Lethinen <lehtinenjori03@gmail.com>
2026-07-26 13:47:19 +0300

    chore: ideas

--- src/c++/auxiliary/find_strip_by_sequence_point/index.hpp
+++ src/c++/auxiliary/find_strip_by_sequence_point/index.hpp
@@ -5,8 +5,13 @@
 #include <cstdint>
 #include <tuple>
 
-// consider bucketeering by `realm_tag` hash and then linear searching trough
-// `realm_count` as here is done for entire sequence points
+// consider narrowing search trough bucketeering by `realm_tag` hash and then
+// linear searching trough `realm_count` as here is done for entire sequence
+// points.
+
+// Maybe an AoS stored by the realm_tag hash to a table and then binary search
+// so structs ordered by realm_count at their point and then another field
+// containing the start position in the reel storing the actual strips
 std::tuple<std::uint32_t, std::uint32_t>
 find_strip_by_sequence_point(ProjectorState *projector,
                              const Uint128 *sequence_point) noexcept {

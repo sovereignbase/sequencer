f56b6ed3b2b384f8aa54d9972d4d2dbd8995097c
Jori Lethinen <lehtinenjori03@gmail.com>
2026-08-09 17:46:19 +0300

    chore: prepare path for values() method

--- src/c++/classes/length_table/index.hpp
+++ src/c++/classes/length_table/index.hpp
@@ -12,17 +12,7 @@ private:
 public:
   inline void adjust_chekpoints(bool remove, std::uint32_t after_index,
                                 std::uint32_t length,
-                                Projector *projector) noexcept {
-    if (remove) {
-      // Start from the checkpoint at/before after_index.
-      // Walk projection forward by `length`,
-      // updating affected checkpoints left -> right.
-    } else {
-      // Start from the checkpoint at/after the affected
-      // range. Walk projection backward by `length`,
-      // updating affected checkpoints right -> left.
-    }
-  }
+                                Projector *projector) noexcept {}
 
   [[nodiscard]] inline std::pair<std::uint32_t, bool>
   nearest_chekpoint(std::uint32_t index) const noexcept {

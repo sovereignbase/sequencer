adba65944e0bd0ab1f6f6e75c3486729c1d00a04
Jori Lethinen <lehtinenjori03@gmail.com>
2026-08-12 00:36:01 +0300

    chore: resolve final stress issues

--- src/c++/algorithms/mask_strip/index.hpp
+++ src/c++/algorithms/mask_strip/index.hpp
@@ -50,7 +50,6 @@ mask_strip(Projector *projector, std::uint32_t containing_strip_index,
                            projector);
     projection_frame_index = projector->gate_projection_frame_index + offset;
   }
-
   std::uint32_t remaining_frame_count = incoming_strip.frame_count;
   std::uint32_t removed_frame_count = 0;
 
@@ -83,6 +82,16 @@ mask_strip(Projector *projector, std::uint32_t containing_strip_index,
 
   projector->length_table.adjust_checkpoints<Projector>(
       projector, projection_frame_index, removed_frame_count, true);
+  if (projection_frame_index == 0 &&
+      removed_frame_count != projector->projection_frame_count) {
+    projector->strips[projector->length_table.nearest_checkpoint(0).first]
+        .checkpoint_projection_frame_index = u32_max;
+    std::uint32_t first_strip_index = projector->right[containing_strip_index];
+    while (projector->strips[first_strip_index].is_masked != 0)
+      first_strip_index = projector->right[first_strip_index];
+    projector->length_table.set_first(first_strip_index);
+    projector->strips[first_strip_index].checkpoint_projection_frame_index = 0;
+  }
   projector->projection_frame_count -= removed_frame_count;
 
   return projection_frame_index;
